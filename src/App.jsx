03c3e1d07a09f78e264f94302f8e12c2897a77af
Ajarek <ajarek@poczta.onet.pl>
2023-07-18 21:48:47 +0200

    initial & final commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Users from './Users'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Build React Drag N Drop With Dnd Kit</h1>
+      <Users />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Anna',
+  },
+  {
+    id: 2,
+    name: 'Grzegorz',
+  },
+  {
+    id: 3,
+    name: 'Monika',
+  },
+  {
+    id: 4,
+    name: 'Andrzej',
+  },
+  {
+    id: 5,
+    name: 'Dorota',
+  },
+  {
+    id: 6,
+    name: 'Stanisław',
+  },
+  {
+    id: 7,
+    name: 'Felix',
+  },
+  {
+    id: 8,
+    name: 'Jakub',
+  },
+]
